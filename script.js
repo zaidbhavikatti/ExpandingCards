5226e5d6d0d33f344668a96623ef195374670403
zaidbhavikatti <98631203+zaidbhavikatti@users.noreply.github.com>
2022-05-06 22:12:51 +0530

    2nd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach(panel => {
+panels.forEach( panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
