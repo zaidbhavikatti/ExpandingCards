4903864c7c35eeab9ad65ea3b846bd73586d2678
zaidbhavikatti <98631203+zaidbhavikatti@users.noreply.github.com>
2022-05-08 13:12:34 +0530

    second

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach( panel => {
+panels.forEach(( panel) => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
-    })
-}
+    });
+});
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
-    })
+    });
 }
+
+
+// let panels = document.querySelectorAll(".panel");
+
+// panels.forEach((panel) => {
+//   panel.addEventListener("click", () => {
+//     removeActive();
+//     panel.classList.add("active");
+//   });
+// });
+
+// function removeActive() {
+//   panels.forEach((panel) => {
+//     panel.classList.remove("active");
+//   });
+// }
